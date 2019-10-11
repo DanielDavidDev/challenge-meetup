b115432b4b3c6582d1b71d858bfe4227e2df30dc
Daniel Felizardo <daniel.david772@gmail.com>
2019-10-11 03:44:29 -0300

    refactor: configs files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,51 @@
 module.exports = {
   env: {
+    browser: true,
     es6: true,
     node: true,
     jest: true,
   },
-  extends: [
-    'airbnb-base',
-    'prettier'
-  ],
-  plugins:[
-    'prettier'
-  ],
+  extends: ['airbnb-base', 'prettier', 'airbnb', 'prettier/react'],
+  plugins: ['prettier', 'react', 'import', 'jsx-a11y', 'react-hooks'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
+    __DEV__: true,
   },
+  parser: 'babel-eslint',
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 2018,
     sourceType: 'module',
   },
   rules: {
-    'prettier/prettier': 'error',
+    'prettier/prettier': 'off',
     'class-methods-use-this': 'off',
     'no-param-reassign': 'off',
-    'camelcase': 'off',
-    'no-unused-vars': ['error',{'argsIgnorePattern': 'next'}],
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
     'no-console': 'off',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'react/jsx-one-expression-per-line': 'off',
+    'global-require': 'off',
+    'react-native/no-raw-text': 'off',
+    'no-param-reassign': 'off',
+    'no-underscore-dangle': 'off',
+    camelcase: 'off',
+    'no-console': ['error', { allow: ['tron'] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-props-no-spreading': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
   },
 };
